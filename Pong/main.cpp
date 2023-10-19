497262a9d7539e818a77043d29fd7069235cc717
gzhao10 <68718446+gzhao10@users.noreply.github.com>
2023-10-18 23:01:03 -0400

    changed due date in header

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,7 +1,7 @@
 /**
 * Author: Gavin Zhao
 * Assignment: Pong Clone
-* Date due: 2023-10-14, 11:59pm
+* Date due: 2023-10-21, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
